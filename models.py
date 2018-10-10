19f892d24ccb724c3d050f7bae15051d003f4191
Sri Devan <sria@bgsu.edu>
2018-10-10 12:24:16 -0400

    Add new models to represent the unit_centers and unit_rotations tables

--- models.py
+++ models.py
@@ -4,19 +4,46 @@ class UnitCorrespondence(db.Model):
 
 	__tablename__ = "correspondence_units"
 
-	correspondence_id = db.Column(db.String, primary_key=True)
+	#correspondence_id = db.Column(db.String, primary_key=True)
 	unit_id_1 = db.Column(db.String, primary_key=True)
 	unit_id_2 = db.Column(db.String, primary_key=True)
+	pdb_id_1 = db.Column(db.String, primary_key=True)
+	pdb_id_2 = db.Column(db.String, primary_key=True)
 
-	def __init__(self, unit_id_1, unit_id_2):
-		self.unit_id_1 = unit_id_1
-		self.unit_id_2 = unit_id_2
+	#def __init__(self, unit_id_1, unit_id_2, pdb_id_1, pdb_id_2):
+		#self.unit_id_1 = unit_id_1
+		#self.unit_id_2 = unit_id_2
+		#self.pdb_id_1 = pdb_id_1
+		#self.pdb_id_2 = pdb_id_2
 
-	def __repr__(self):
-		return "{}:{}".format(self.unit_id_1, self.unit_id_2)
-		#equivalent_residues = {self.unit_id_1: self.unit_id_2}
-		#return equivalent_residues
-		
+class UnitCenters(db.Model):
 
+	__tablename__ = "unit_centers"
+
+	#correspondence_id = db.Column(db.String, primary_key=True)
+	unit_center_id = db.Column(db.Integer, primary_key=True)
+	unit_id = db.Column(db.String)
+	pdb_id = db.Column(db.String)
+	name = db.Column(db.String)
+	x = db.Column(db.Float)
+	y = db.Column(db.Float)
+	z = db.Column(db.Float)
+
+class UnitRotations(db.Model):
+
+	__tablename__ = "unit_rotations"
+
+	#correspondence_id = db.Column(db.String, primary_key=True)
+	unit_id = db.Column(db.String, primary_key=True)
+	pdb_id = db.Column(db.String)
+	cell_0_0 = db.Column(db.Float)
+	cell_0_1 = db.Column(db.Float)
+	cell_0_2 = db.Column(db.Float)
+	cell_1_0 = db.Column(db.Float)
+	cell_1_1 = db.Column(db.Float)
+	cell_1_2 = db.Column(db.Float)
+	cell_2_0 = db.Column(db.Float)
+	cell_2_1 = db.Column(db.Float)
+	cell_2_2 = db.Column(db.Float)
 
     
