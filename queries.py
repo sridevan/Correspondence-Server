22df73f27c48346bd9bbbc9ffb94aa883e689605
Sri Devan <sria@bgsu.edu>
2019-03-06 21:53:01 -0500

    Commented out unused code

--- queries.py
+++ queries.py
@@ -3,23 +3,4 @@ This file will contain the logic for getting and processing
 database queries
 """
 
-def ranges(data, pdb, chain):
-
-    units_list = []
-
-    if len(data) > 2:
-
-        for range in data:
-            units_list.append(range[0])
-
-        '''
-        for range in data:
-            units_query = UnitInfo.query.filter_by(pdb_id=pdb, chain=chain). \
-                filter(UnitInfo.chain_index.between(range[0], range[1])) \
-                .order_by(UnitInfo.chain_index).all()
-
-            for row in units_query:
-                units_list.append(row.unit_id)
-        '''
-
-    return units_list
+#def get_nr_class(ife):
