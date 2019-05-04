918af43d314a2b766c8d35512dd62013e9a1d62b
sridevan <sria@bgsu.edu>
2019-05-04 17:19:38 -0400

    Added a wsgi file for the application

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import logging
+
+sys.path.append(os.path.dirname(__file__))
+
+
+from app import app as application
+
+#logging.basicConfig(stream=sys.stdout)
+#logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
